75595bc6f3b237c06a4aa97ea64b1fff94760aec
Austin Mason <austinpmason@gmail.com>
2016-12-06 16:02:03 +0000

    create remember_digest

--- members-only/app/models/user.rb
+++ members-only/app/models/user.rb
@@ -1,7 +1,26 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+  
+  before_create :create_user_token
+  
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
                     uniqueness: { case_sensitive: false } 
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+  
+  def User.digest(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+  
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+  
+  private
+  
+  def create_user_token
+    self.remember_digest = User.digest(User.new_remember_token)
+  end
+  
 end
